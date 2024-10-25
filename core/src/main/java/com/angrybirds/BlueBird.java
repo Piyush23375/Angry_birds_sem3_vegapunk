b42eee1e32c1255f666e57713f6d7a1a19be1bb4
Piyush23375 <147907430+Piyush23375@users.noreply.github.com>
2024-10-25 09:45:29 +0530

    Update BlueBird.java

--- core/src/main/java/com/angrybirds/BlueBird.java
+++ core/src/main/java/com/angrybirds/BlueBird.java
@@ -2,8 +2,8 @@ package com.angrybirds;
 
 public class BlueBird extends Bird {
 
-    // Constructor for BlueBird, using the Bird's constructor
+   
     public BlueBird(float x, float y, float scale) {
-        super("blue_bird.png", x, y, scale); // Pass texture and other parameters
+        super("blue_bird.png", x, y, scale); 
     }
 }
