682a4ea0a98ad46085199ae666f3798d97fd4923
Piyush23375 <147907430+Piyush23375@users.noreply.github.com>
2024-10-25 09:53:15 +0530

    Update Main.java

--- core/src/main/java/com/angrybirds/Main.java
+++ core/src/main/java/com/angrybirds/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 public class Main extends Game {
     @Override
     public void create() {
-        // Set the initial screen when the application starts
+       
         setScreen(new MainMenuScreen(this));
     }
 }
