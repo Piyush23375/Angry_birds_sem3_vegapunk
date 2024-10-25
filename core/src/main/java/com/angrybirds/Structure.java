9018ba759915c93918fe7da924386b8926296712
Ryan Mittal <138594872+ryan07671@users.noreply.github.com>
2024-10-25 09:45:18 +0530

    Update Structure.java

--- core/src/main/java/com/angrybirds/Structure.java
+++ core/src/main/java/com/angrybirds/Structure.java
@@ -1,1 +1,39 @@
+package com.angrybirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Structure implements GameObject {
+    private Texture texture;
+    private Vector2 position;
+    private float width;
+    private float height;
+
+    public Structure(String texturePath, float x, float y, float scale) {
+        this.texture = loadTexture(texturePath);
+        this.position = new Vector2(x, y);
+        this.width = texture.getWidth() * scale;
+        this.height = texture.getHeight() * scale;
+    }
+
+    private Texture loadTexture(String texturePath) {
+        return new Texture(Gdx.files.internal(texturePath));
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        batch.draw(texture, position.x, position.y, width, height);
+    }
+
+    @Override
+    public void dispose() {
+        disposeTexture();
+    }
+
+    private void disposeTexture() {
+        texture.dispose();
+    }
+}
 
